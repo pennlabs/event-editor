6bdf0bc69f07d71834e76cacf81a3a6a3a9b470b
Eric Wang <ezwang1@gmail.com>
2018-03-13 21:57:19 -0400

    fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.paperclip_defaults = {
-    storage: s3,
+    storage: :s3,
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
