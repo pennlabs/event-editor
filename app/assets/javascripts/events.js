54b6c6eafe545201441b6861c6749a84461caec3
Eric Wang <ezwang1@gmail.com>
2018-03-17 13:42:41 -0400

    better select

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -31,10 +31,37 @@ $(document).on("turbolinks:load", function() {
         enableTime: true,
         dateFormat: 'Y-m-d H:i:S'
     });
+
+    if ($("#event_end_time").val()) {
+        var diff = (Date.parse($("#event_end_time").val()) - Date.parse($("#event_start_time").val())) / 1000;
+        var selected_button = $("#quick-buttons .btn[data-value='" + diff + "']");
+        if (selected_button.length) {
+            selected_button.addClass("btn-primary").removeClass("btn-light");
+        }
+        else {
+            $("#end-time-wrapper").show();
+            $("#duration-wrapper").hide();
+        }
+    }
+
     $("#quick-buttons .btn").click(function(e) {
         e.preventDefault();
-        if (!$("#event_start_time").val()) return;
-        var time = parseInt($(this).attr("data-value"));
+        var raw = $(this).attr("data-value");
+        if (raw == "other") {
+            $("#duration-wrapper").slideUp();
+            $("#end-time-wrapper").slideDown();
+            return;
+        }
+        if (!$("#event_start_time").val()) {
+            $("#event_start_time").addClass("is-invalid");
+            setTimeout(function() {
+                $("#event_start_time").removeClass("is-invalid");
+            }, 500);
+            return;
+        }
+        $("#quick-buttons .btn").addClass("btn-light").removeClass("btn-primary");
+        $(this).addClass("btn-primary").removeClass("btn-light");
+        var time = parseInt(raw);
         var start = Date.parse($("#event_start_time").val());
         var end = new Date(start + time * 1000);
         end_time.setDate(end);
